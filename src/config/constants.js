ae165281bcef333e4149953d5893a9a06fd06266
Brenda Long <brenda@studio6thave.com>
2018-09-04 12:46:39 -0500

    updated db

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,9 +1,7 @@
 import Rebase from 're-base';
 import firebase from 'firebase';
 const app = firebase.initializeApp({
-   apiKey: "AIzaSyA60-RA1ZHZRXrmmAH-gsp1h7OpRFYWugY",
-   authDomain: "d18-demo.firebaseapp.com",
-   databaseURL: "https://d18-demo.firebaseio.com"
+
 });
 export const rebase = Rebase.createClass(app.database());
 
